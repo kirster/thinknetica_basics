5fa9a129dd2ee93756d1f1966c1f4184f356f93e
kirster <kir.sazonov@gmail.com>
2017-10-18 08:20:30 +0300

    fix mistake for lesson_6

--- lesson_6/route.rb
+++ lesson_6/route.rb
@@ -3,7 +3,18 @@ class Route
 
   def initialize(initial_station, final_station)
     @route_code = "#{initial_station.title}_#{final_station.title}"
+    validate!
     @stations = [initial_station, final_station]
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  def validate!
+    raise "Route should consist of station objects" unless initial_station.is_a?(Station) && final_station.is_a?(Station)
   end
 
   def add_station(station)
