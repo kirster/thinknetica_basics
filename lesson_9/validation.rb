e18c54ae13d67ddae97e5fae6881508c7c7d2164
kirster <kir.sazonov@gmail.com>
2017-10-27 16:07:18 +0300

    fix mistakes in validation module

--- lesson_9/validation.rb
+++ lesson_9/validation.rb
@@ -8,8 +8,9 @@ module Validation
     attr_reader :rules
 
     def validate(var_name, validation_type, option = nil)
-      @rules = []
-      @rules << { var_name: var_name,
+      @rules ||= []
+      @rules << { var_properties: { var_name: var_name, 
+                                    var_value: instance_variable_get("@#{var_name}") },
                   validation_type: validation_type.to_sym,
                   option: option }
     end
@@ -24,23 +25,28 @@ module Validation
 
     def validate!
       self.class.rules.each do |rule|
-        send rule[:validation_type], rule[:var_name], rule[:option]
+        send rule[:validation_type], rule[:var_properties], rule[:option]
       end
       true
     end
 
-    def presence(var_name, _option)
-      if var_name.nil? || var_name.empty?
-        raise 'Parameter can`t be nil or empty(for String class)'
+    def presence(var_properties, _option)
+      if var_properties[:var_value].nil? || var_properties[:var_value].empty?
+        raise "Parameter #{var_properties[:var_name]} \
+              can`t be nil or empty(for String class)"
       end
     end
 
-    def type(var_name, option)
-      raise 'Parameter has wrong type' unless var_name.is_a?(option)
+    def type(var_properties, option)
+      unless var_properties[:var_value].is_a?(option)
+        raise "Parameter #{var_properties[:var_name]} has wrong type"
+      end
     end
 
-    def format(var_name, option)
-      raise 'Parameter has wrong format' if var_name !~ option
+    def format(var_properties, option)
+      if var_properties[:var_value] !~ option
+        raise "Parameter #{var_properties[:var_name]} has wrong format"
+      end
     end
   end
 end
